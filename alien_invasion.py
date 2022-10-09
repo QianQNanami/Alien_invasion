b5da0ce62b66b456fbebbc717e3bee710ce6b3af
expect2004 <1029922060@qq.com>
2022-10-09 22:34:58 +0800

    Rewrite alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,33 @@
+from email.mime import image
+import imp
 import sys
 import pygame
+
+from settings import Settings
 
 class AlienInvasion:
     
     def __init__(self):
         pygame.init()
         
-        self.screen = pygame.display.set_mode((1200, 700))
-        pygame.display.set_caption("Alien Invasion")
+        self.settings = Settings()
+        # Initize the surface
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        # self.screen = pygame.display.set_mode((1200, 700))
+        # pygame.display.set_caption("Alien Invasion")
         
+        # Set the Background Color
+        # self.bg_color = (230, 230, 230)
+                
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if(event.type == pygame.QUIT):
                     sys.exit()
-                    
+            
+            # Recolor the screen every circle
+            self.screen.fill(self.settings.bg_color)
+            
             pygame.display.flip()
 
 
